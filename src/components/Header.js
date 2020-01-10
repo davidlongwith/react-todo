652d2c750e9e4e54b02de6f2f3333d0510674a9b
David Longwith <david@davidlongwith.com>
2020-01-09 19:22:37 -0500

    create header component

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import logo from "../logo.svg";
+
+const Header = () => (
+  <header className="App-header">
+    <img src={logo} className="App-logo" alt="logo" />
+    <h1>React To-Do</h1>
+    <p>Form Input Here</p>
+  </header>
+);
+
+export default Header;
