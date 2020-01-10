98c01ddabb80310a250fa865d27ee9857630f586
David Longwith <david@davidlongwith.com>
2020-01-09 20:10:33 -0500

    convert app to class

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from "react";
+import { Component } from "react";
 import { v4 } from "uuid";
 import "./App.css";
 import Header from "./components/Header.js";
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      {console.log(v4())}
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        {console.log(v4())}
+      </div>
+    );
+  }
 }
 
 export default App;
