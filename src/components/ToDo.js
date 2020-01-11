1d0ef879288d35a7ee1288fc86e47df97e03c8c4
David Longwith <david@davidlongwith.com>
2020-01-11 16:14:23 -0500

    todo css tweaks

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const ToDo = ({ todo, onRemove = f => f }) => (
-  <section className="todo-container">
+  <section className="todo-entry-container">
     <button onClick={onRemove}>-</button>
     <p>{todo}</p>
   </section>
--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -4,7 +4,7 @@ import ToDo from "./ToDo.js";
 const ToDoList = ({ todos, onRemove = f => f }) => (
   <div className="todo-list">
     {todos.length === 0 ? (
-      <p>No To-Do's Listed. (Add a To-Do)</p>
+      <p className="no-todos">No To-Do's Listed. (Add a To-Do)</p>
     ) : (
       todos.map(todo => (
         <ToDo key={todo.id} {...todo} onRemove={() => onRemove(todo.id)} />
