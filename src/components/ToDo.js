ed9815a69eb0c339b710f864af82e0b546cb271f
David Longwith <david@davidlongwith.com>
2020-01-12 00:23:53 -0500

    remove default prop on ToDo.js

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -2,7 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 
 // send onClick event to parent component with "onRemove" callback function prop
-const ToDo = ({ todo, onRemove = f => f }) => (
+const ToDo = ({ todo, onRemove }) => (
   <section className="todo-entry-container">
     <button onClick={onRemove}>x</button>
     <p>{todo}</p>
@@ -12,9 +12,6 @@ const ToDo = ({ todo, onRemove = f => f }) => (
 // Typechecking with PropTypes
 ToDo.propTypes = {
   todo: PropTypes.string
-};
-ToDo.defaultProps = {
-  onRemove: f => f
 };
 
 export default ToDo;
