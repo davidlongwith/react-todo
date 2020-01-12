772e8649611571d3d3be1b1c94a032e35f921962
David Longwith <david@davidlongwith.com>
2020-01-11 20:36:22 -0500

    fix remove button text centering

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const ToDo = ({ todo, onRemove = f => f }) => (
   <section className="todo-entry-container">
-    <button onClick={onRemove}>-</button>
+    <button onClick={onRemove}>x</button>
     <p>{todo}</p>
   </section>
 );
