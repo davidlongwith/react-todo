582589f2a9db0206e7e205c43b0752e31f1e5cab
David Longwith <david@davidlongwith.com>
2020-01-14 22:00:48 -0500

    comments...

--- src/App.js
+++ src/App.js
@@ -5,8 +5,16 @@ import "./App.css";
 import Header from "./components/Header.js";
 import ToDoList from "./components/ToDoList.js";
 
-// Root application ES6 component class with constructor and custom methods.  All application
-// state managed here.
+/*
+ * Root application ES6 component class with constructor and custom methods.  All application
+ * state managed here.
+ *
+ * <Header /> contains the user input form for new todos and invokes addToDo()
+ * using a callback function property when the form is submitted.
+ *
+ * <ToDoList /> recieves the full todos array with prop "todos", and invokes removeToDo()
+ * when the callback function property "onRemove" is activated by the remove todo button event.
+ */
 export default class App extends Component {
   constructor(props) {
     super(props);
