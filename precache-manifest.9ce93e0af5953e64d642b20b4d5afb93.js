1922587c12b74cc828e78379898fb40183960ff8
David Longwith <david@davidlongwith.com>
2020-01-14 22:33:31 -0500

    Updates

---
+++ precache-manifest.9ce93e0af5953e64d642b20b4d5afb93.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33a8e948dd5e4474f118dd5f985efba7",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "30be79116cc716be89dd",
+    "url": "/react-todo/static/css/main.e7873ed3.chunk.css"
+  },
+  {
+    "revision": "1fdb429b91f625d1ab18",
+    "url": "/react-todo/static/js/2.4d715747.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-todo/static/js/2.4d715747.chunk.js.LICENSE"
+  },
+  {
+    "revision": "30be79116cc716be89dd",
+    "url": "/react-todo/static/js/main.60290a46.chunk.js"
+  },
+  {
+    "revision": "4d6de7b78de98614c129",
+    "url": "/react-todo/static/js/runtime-main.01144c30.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-todo/static/media/logo.5d5d9eef.svg"
+  }
+]);
