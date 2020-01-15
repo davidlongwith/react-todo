cbdd9ef85c2094da694d8fd6f41a3d05d9e8d079
David Longwith <david@davidlongwith.com>
2020-01-14 19:52:53 -0500

    convert new todo form to stateless functional component

--- src/components/AddToDoForm.js
+++ src/components/AddToDoForm.js
@@ -1,44 +1,38 @@
 import React from "react";
-import { Component } from "react";
 import "../AddToDoForm.css";
 import PropTypes from "prop-types";
 
-class AddToDoForm extends Component {
-  constructor(props) {
-    super(props);
-    this.submit = this.submit.bind(this);
-  }
-  submit(e) {
-    const { _todo } = this.refs;
+// This stateless functional component displays a form for the user to
+// enter new todo's.  The new todo is sent up the tree using the "onNewToDo"
+// callback function property.
+const AddToDoForm = ({ onNewToDo = f => f }) => {
+  let _todo;
+
+  const submit = e => {
     e.preventDefault();
-    // pass new todo input ref value up to parent component with callback function property "onNewToDo"
-    this.props.onNewToDo(_todo.value);
+    onNewToDo(_todo);
     _todo.value = "";
     _todo.focus();
-  }
-  render() {
-    return (
-      <div className="add-todo-form-container">
-        <form className="add-todo-form" onSubmit={this.submit}>
-          <input
-            ref="_todo"
-            type="text"
-            placeholder="Add new todo..."
-            required
-          />
-          <button>+</button>
-        </form>
-      </div>
-    );
-  }
-}
+  };
+
+  return (
+    <div className="add-todo-form-container">
+      <form className="add-todo-form" onSubmit={submit}>
+        <input
+          ref={input => (_todo = input)} // save ref to local variable
+          type="text"
+          placeholder="Add new todo..."
+          required
+        />
+        <button>+</button>
+      </form>
+    </div>
+  );
+};
 
 // Typechecking with PropTypes
 AddToDoForm.propTypes = {
   onNewToDo: PropTypes.func
-};
-AddToDoForm.defaultProps = {
-  onNewToDo: f => f
 };
 
 export default AddToDoForm;
